2746346ce4fac55d4ae4553e75b86591b002c15c
Harsh-HK7 <harshkalal746@gmail.com>
2025-06-01 15:07:26 +0530

    Add files via upload

---
+++ src/components/clock.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState} from "react";
+
+const clock = () => {
+
+    const [currentTime , setCurrentTime] = useState(new Date());
+
+    useEffect(()=>{
+        const timer = setInterval(()=>{
+            setCurrentTime(new Date());
+        }, 1000)
+        return () => clearInterval(timer)
+    })
+
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-5xl md:text-7xl font-bold">{new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false })}</h1>
+      <p className="text-sm md:text-md font-medium">{new Date().toLocaleDateString()}</p>
+    </div>
+  )
+};
+export default clock
